606bd513a918ce7a74832e3906c242ba93205f32
Nur Sidik <nursidik@pinteraktif.com>
2025-03-20 09:26:04 +0700

    Add Response API add API response to make it clean add new DTO to create and update user add hash password to make it secure

--- netcore-api-template/Controllers/UserController.cs
+++ netcore-api-template/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,53 +12,167 @@ namespace netcore_api_template.Controllers;
 
 [ApiController]
 [Route("api/users")]
-public class UserController(IUserService userService) : ControllerBase
+public class UserController(
+    IUserService userService,
+    IMapper _mapper
+    ) : ControllerBase
 {
     [HttpGet]
     [Authorize]
     public async Task<IActionResult> GetUsers()
     {
-        var users = await userService.All();
-        return Ok(users);
+        try
+        {
+            var users = await userService.All();
+            return Ok(new ApiResponse<IEnumerable<UserDataDto>>
+            {
+                Success = true,
+                Data = users,
+                Message = "List user retrieved successfully."
+            });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(
+                new ApiResponse<IEnumerable<string>>
+                {
+                    Success = false,
+                    Data = null,
+                    Message = "Error: " + ex.Message
+                }
+            );
+        }
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> GetById(Guid id)
     {
-        if (id == Guid.Empty)
-            return BadRequest("Invalid user id.");
+        try
+        {
+            if (id == Guid.Empty)
+                return BadRequest(new ApiResponse<IEnumerable<string>>
+                {
+                    Success = false,
+                    Data = null,
+                    Message = "Invalid user id."
+                });
 
-        var user = await userService.GetById(id);
-        return Ok(user);
+            var user = await userService.GetById(id);
+            return Ok(
+                new ApiResponse<UserDataDto>
+                {
+                    Success = true,
+                    Data = user,
+                    Message = "Get user retrieved successfully."
+                }
+            );
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(
+                new ApiResponse<IEnumerable<string>>
+                {
+                    Success = false,
+                    Data = null,
+                    Message = "Error: " + ex.Message
+                }
+            );
+        }
     }
 
     [HttpPost]
-    public async Task<IActionResult> Create(User user)
+    public async Task<IActionResult> Create(CreateUserDto createUserDto)
     {
-        user.CreatedAt = DateTime.UtcNow;
-        user.Password = PasswordHasher.HashPassword(user.Password);
-        var result = await userService.Create(user);
-        return Ok(result);
+        try
+        {
+            var dtCreate = _mapper.Map<User>(createUserDto);
+
+            dtCreate.CreatedAt = DateTime.UtcNow;
+            dtCreate.Password = PasswordHasher.HashPassword(dtCreate.Password);
+            dtCreate.CreatedBy = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            var result = await userService.Create(dtCreate);
+            // return Ok(result);
+            return CreatedAtAction(
+                        nameof(GetById),
+                        new { id = result },
+                        new ApiResponse<CreateUserDto>
+                        {
+                            Success = true,
+                            Data = createUserDto,
+                            Message = "User created successfully."
+                        }
+                    );
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(
+                new ApiResponse<IEnumerable<string>>
+                {
+                    Success = false,
+                    Data = null,
+                    Message = "Error: " + ex.Message
+                }
+            );
+        }
+
     }
 
     [HttpPut]
-    public async Task<IActionResult> Update(User user)
+    public async Task<IActionResult> Update(UpdateUserDto updateUserDto)
     {
-        if (user.Id == Guid.Empty)
-            return BadRequest("Invalid user id.");
+        try
+        {
+            if (updateUserDto.Id == Guid.Empty)
+                return BadRequest(new ApiResponse<IEnumerable<string>>
+                {
+                    Success = false,
+                    Data = null,
+                    Message = "Invalid user id."
+                });
 
-        if (user.Password != null)
-            user.Password = PasswordHasher.HashPassword(user.Password);
 
-        user.UpdatedAt = DateTime.UtcNow;
-        var result = await userService.Update(user);
-        return Ok(result);
+            var dtUpdate = _mapper.Map<User>(updateUserDto);
+
+            if (dtUpdate.Password != null)
+                dtUpdate.Password = PasswordHasher.HashPassword(dtUpdate.Password);
+
+            dtUpdate.UpdatedAt = DateTime.UtcNow;
+            dtUpdate.UpdatedBy = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier)!);
+            var result = await userService.Update(dtUpdate);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(
+                new ApiResponse<IEnumerable<string>>
+                {
+                    Success = false,
+                    Data = null,
+                    Message = "Error: " + ex.Message
+                }
+            );
+        }
+
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(Guid id)
     {
-        var result = await userService.Delete(id);
-        return Ok(result);
+        try
+        {
+            var result = await userService.Delete(id);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(
+                new ApiResponse<IEnumerable<string>>
+                {
+                    Success = false,
+                    Data = null,
+                    Message = "Error: " + ex.Message
+                }
+            );
+        }
     }
 }
