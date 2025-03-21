6112c71f9003cc993dac3985ff13f2f7619c4f94
Nur Sidik <nursidik@pinteraktif.com>
2025-03-21 07:20:33 +0700

    Add pagination add pagination query params, sort and filter

--- netcore-api-template/Models/User.cs
+++ netcore-api-template/Models/User.cs
@@ -29,8 +29,10 @@ public class UserDataDto
     public required string Email { get; set; }
     public required DateTime CreatedAt { get; set; }
     public Guid CreatedBy { get; set; }
-    public DateTime UpdatedAt { get; set; }
+    public string? CreatedByName { get; set; }
+    public DateTime? UpdatedAt { get; set; }
     public Guid UpdatedBy { get; set; }
+    public string? UpdatedByName { get; set; }
     public bool IsActive { get; set; }
 }
 
